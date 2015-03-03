179b0daf14ecff624598b5a85fc06ece2104d122
Loran Briggs <loranbriggs@gmail.com>
2015-03-03 09:36:14 -0800

    when clicking on table element, displays its children

--- astro.go
+++ astro.go
@@ -10,8 +10,6 @@ import (
   _ "github.com/nakagami/firebirdsql"
 	"github.com/elgs/gosqljson"
   "github.com/gorilla/mux"
-  "github.com/PuerkitoBio/goquery"
-  "net/url"
 )
 
 var username = "sysdba"
@@ -29,58 +27,57 @@ func home(w http.ResponseWriter, r *http.Request) {
 func querydb(w http.ResponseWriter, r *http.Request) {
   vars  := mux.Vars(r)
   table := vars["table"]
-  idparm    := vars["id"]
-  id, notInt := strconv.Atoi(idparm)
 	conn, _ := sql.Open("firebirdsql", url)
 	defer conn.Close()
-  var query string
-  if notInt == nil {
-    // we have an id
-    query = fmt.Sprintf("SELECT * FROM %s WHERE id=%d", table, id)
-  } else {
-    // else list them all
-    query = fmt.Sprintf("SELECT * FROM %s", table)
-  }
+  query := fmt.Sprintf("SELECT * FROM %s", table)
 	a, _ := gosqljson.QueryDbToMapJson(conn, "lower", query, 0, 3)
 	fmt.Fprintf(w, "%s", a)
 	//conn.QueryRow("SELECT Count(*) FROM STARS").Scan(&n)
     //fmt.Fprintf(w, "Found %v entries in %s", n, table)
 }
 
-func getImages(w http.ResponseWriter, r *http.Request) {
+func queryind(w http.ResponseWriter, r *http.Request) {
   vars  := mux.Vars(r)
-  url := vars["url"]
-  fmt.Println(url)
-
-  doc, err := goquery.NewDocument(url) 
-  if err != nil {
-    log.Fatal(err)
+  table := vars["table"]
+  idparm    := vars["id"]
+  id, _ := strconv.Atoi(idparm)
+  conn, _ := sql.Open("firebirdsql", url)
+  defer conn.Close()
+  var query string
+  var child string
+  var attr  string
+  switch table {
+    case "planets":
+      child = "moons"
+      attr  = "parent"
+      break
+    case "stars":
+      child = "planets"
+      attr  = "parent"
+      break
+    case "solar_systems":
+      child = "stars"
+      attr  = "parent"
+      break
+    case "galaxies":
+      child = "solar_systems"
+      attr  = "parent"
+    default:
+      child = table
+      attr  = "id"
   }
-
-  var src1 string
-  //var src2 string
-
-  doc.Find("img").Each(func(i int, s *goquery.Selection) {
-    if i == 1 {
-      var srca, _ = s.Attr("src")
-      src1 = srca
-    }
-    /*
-    if i == 2 {
-      var srcb, _ = s.Attr("src")
-      src2 = srcb
-    }
-    */
-  })
-  fmt.Fprintf(w, "%s", url.QueryEscape(src1))
+  query = fmt.Sprintf("SELECT * FROM %s WHERE %s=%d", child, attr, id)
+  a, _ := gosqljson.QueryDbToMapJson(conn, "lower", query, 0, 3)
+  fmt.Fprintf(w, "%s", a)
+  //conn.QueryRow("SELECT Count(*) FROM STARS").Scan(&n)
+    //fmt.Fprintf(w, "Found %v entries in %s", n, table)
 }
 
 func main() {
   r := mux.NewRouter().StrictSlash(true)
   r.HandleFunc("/", home)
   r.HandleFunc("/{table}", querydb)
-  r.HandleFunc("/{table}/{id}", querydb)
-  r.HandleFunc("/getimages/{url}" getImages)
+  r.HandleFunc("/{table}/{id}", queryind)
   http.Handle("/", r)
   fmt.Println("listing at 4444....")
   http.ListenAndServe(":4444", nil)
