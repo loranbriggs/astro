27c4e7f55b66de8c3784462749edcbdf6d6b9e91
Loran Briggs <loranbriggs@gmail.com>
2015-03-03 07:19:28 -0800

    urls are now links on front end. plus an experimental image getter, gets url but cant display to front end due to JS security details

--- astro.go
+++ astro.go
@@ -10,6 +10,8 @@ import (
   _ "github.com/nakagami/firebirdsql"
 	"github.com/elgs/gosqljson"
   "github.com/gorilla/mux"
+  "github.com/PuerkitoBio/goquery"
+  "net/url"
 )
 
 var username = "sysdba"
@@ -17,8 +19,6 @@ var password = "masterkey"
 var path     = "/Users/loran/firebird/testdb/test.fdb"
 //var path     = "C:/Users/toram_000/firebird/testdb/test.fdb";
 var url      = fmt.Sprintf("%s:%s@localhost%s", username, password, path)
-
-fmt.Println(runtime.GOOS)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("views/home.html")
@@ -47,11 +47,40 @@ func querydb(w http.ResponseWriter, r *http.Request) {
     //fmt.Fprintf(w, "Found %v entries in %s", n, table)
 }
 
+func getImages(w http.ResponseWriter, r *http.Request) {
+  vars  := mux.Vars(r)
+  url := vars["url"]
+  fmt.Println(url)
+
+  doc, err := goquery.NewDocument(url) 
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  var src1 string
+  //var src2 string
+
+  doc.Find("img").Each(func(i int, s *goquery.Selection) {
+    if i == 1 {
+      var srca, _ = s.Attr("src")
+      src1 = srca
+    }
+    /*
+    if i == 2 {
+      var srcb, _ = s.Attr("src")
+      src2 = srcb
+    }
+    */
+  })
+  fmt.Fprintf(w, "%s", url.QueryEscape(src1))
+}
+
 func main() {
   r := mux.NewRouter().StrictSlash(true)
   r.HandleFunc("/", home)
   r.HandleFunc("/{table}", querydb)
   r.HandleFunc("/{table}/{id}", querydb)
+  r.HandleFunc("/getimages/{url}" getImages)
   http.Handle("/", r)
   fmt.Println("listing at 4444....")
   http.ListenAndServe(":4444", nil)
